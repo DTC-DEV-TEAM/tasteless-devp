5841f432440efedd36265f24c1d15e107804e45f
patrickDigits <patrickpunzalan@digits.ph>
2023-06-11 19:22:40 +0800

    upload columns

--- database/migrations/2023_05_19_075654_create_qr_creations_table.php
+++ database/migrations/2023_05_19_075654_create_qr_creations_table.php
@@ -15,12 +15,16 @@ class CreateQrCreationsTable extends Migration
     {
         Schema::create('qr_creations', function (Blueprint $table) {
             $table->id();
+            $table->string('status_id')->nullable();
             $table->string('campaign_id')->nullable();
             $table->string('gc_description')->nullable();
             $table->integer('gc_value')->nullable();
             $table->integer('number_of_gcs')->nullable();
             $table->string('batch_number')->nullable();
             $table->string('batch_group')->nullable();
+            // $table->string('id_store_concept')->nullable();
+            $table->string('company_id')->nullable();
+            $table->string('upload_limit')->nullable();
             $table->integer('created_by')->nullable();
             $table->integer('updated_by')->nullable();
             $table->timestamps();
