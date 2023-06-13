0d123738290a31240982126c625b3925ae76e625
patrickDigits <patrickpunzalan@digits.ph>
2023-06-13 10:52:59 +0800

    add try and catch

--- app/Jobs/GCListFetchJob.php
+++ app/Jobs/GCListFetchJob.php
@@ -2,15 +2,15 @@
 
 namespace App\Jobs;
 
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
 use App\GCList;
 use App\QrCreation;
+use Illuminate\Bus\Queueable;
 use Illuminate\Support\Facades\Http;
-
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\MaxAttemptsExceededException;
 
 class GCListFetchJob implements ShouldQueue
 {
@@ -36,85 +36,90 @@ class GCListFetchJob implements ShouldQueue
     public function handle()
     {  
 
-        sleep(1);
-        // Prod
-        // $response = Http::withHeaders([
-        //     'Content-Type' => 'application/json',
-        // ])->post('https://devp.digitstrading.ph/api/get-token', [
-        //     'secret' => '9f56aa110c022b17fc1c7cec3fca2016',
-        // ]);
+        try {
+            
+            sleep(1);
+            // Prod
+            // $response = Http::withHeaders([
+            //     'Content-Type' => 'application/json',
+            // ])->post('https://devp.digitstrading.ph/api/get-token', [
+            //     'secret' => '9f56aa110c022b17fc1c7cec3fca2016',
+            // ]);
 
-        // $get_token = $response->json('data.access_token');
+            // $get_token = $response->json('data.access_token');
 
-        // $redemption_list = Http::withHeaders([
-        //     'Authorization' => 'Bearer ' . $get_token['data']['access_token'],
-        // ])->get('https://devp.digitstrading.ph/api/redemption_code');
+            // $redemption_list = Http::withHeaders([
+            //     'Authorization' => 'Bearer ' . $get_token['data']['access_token'],
+            // ])->get('https://devp.digitstrading.ph/api/redemption_code');
 
-        // $gc_list_fetch = $redemption_list->json();
-        
-        // $gc_list_data = array_reverse($gc_list_fetch['data']);
+            // $gc_list_fetch = $redemption_list->json();
+            
+            // $gc_list_data = array_reverse($gc_list_fetch['data']);
 
-        // foreach ($gc_list_data as $item) {
-        //     GCList::firstOrCreate(
-        //         ['id' => $item['id'], 'qr_reference_number' => $item['qr_reference_number']],
-        //         $item
-        //     );
-        // }
+            // foreach ($gc_list_data as $item) {
+            //     GCList::firstOrCreate(
+            //         ['id' => $item['id'], 'qr_reference_number' => $item['qr_reference_number']],
+            //         $item
+            //     );
+            // }
 
-        // Localhost Fetch Gclist
-        $response = Http::withHeaders([
-            'Content-Type' => 'application/json',
-        ])->post('http://127.0.0.1:8080/api/get-token', [
-            'secret' => '84aad301b67368285f7b6f17eed0a064',
-        ]);
+            // Localhost Fetch Gclist
+            $response = Http::withHeaders([
+                'Content-Type' => 'application/json',
+            ])->post('http://127.0.0.1:8080/api/get-token', [
+                'secret' => '84aad301b67368285f7b6f17eed0a064',
+            ]);
 
-        $get_token = $response->json('data.access_token');
+            $get_token = $response->json('data.access_token');
 
-        $redemption_list = Http::withHeaders([
-            'Authorization' => 'Bearer ' . $get_token['data']['access_token'],
-        ])->get('http://127.0.0.1:8080/api/redemption_code');
+            $redemption_list = Http::withHeaders([
+                'Authorization' => 'Bearer ' . $get_token['data']['access_token'],
+            ])->get('http://127.0.0.1:8080/api/redemption_code');
 
-        $gc_list_fetch = $redemption_list->json();
+            $gc_list_fetch = $redemption_list->json();
 
-        if($gc_list_fetch['data']){
+            if($gc_list_fetch['data']){
 
-            foreach ($gc_list_fetch['data'] as $item) {
-                GCList::firstOrCreate(
-                    ['id' => $item['id']],
-                    $item
-                );
+                foreach ($gc_list_fetch['data'] as $item) {
+                    GCList::firstOrCreate(
+                        ['id' => $item['id']],
+                        $item
+                    );
+                }
+            }else{
+                return;
             }
-        }else{
-            return;
-        }
 
-        sleep(1);
+            sleep(1);
 
-        // Localhost fetch campaign
-		$response = Http::withHeaders([
-            'Content-Type' => 'application/json',
-        ])->post('http://127.0.0.1:8080/api/get-token', [
-            'secret' => '84aad301b67368285f7b6f17eed0a064',
-        ]);
+            // Localhost fetch campaign
+            $response = Http::withHeaders([
+                'Content-Type' => 'application/json',
+            ])->post('http://127.0.0.1:8080/api/get-token', [
+                'secret' => '84aad301b67368285f7b6f17eed0a064',
+            ]);
 
-        $get_token = $response->json('data.access_token');
+            $get_token = $response->json('data.access_token');
 
-        $redemption_list = Http::withHeaders([
-            'Authorization' => 'Bearer ' . $get_token['data']['access_token'],
-        ])->get('http://127.0.0.1:8080/api/qr_creation');
+            $redemption_list = Http::withHeaders([
+                'Authorization' => 'Bearer ' . $get_token['data']['access_token'],
+            ])->get('http://127.0.0.1:8080/api/qr_creation');
 
-        $gc_list_fetch = $redemption_list->json();
-        
-        if($gc_list_fetch['data']){
+            $gc_list_fetch = $redemption_list->json();
+            
+            if($gc_list_fetch['data']){
 
-            foreach ($gc_list_fetch['data'] as $item) {
-                QrCreation::firstOrCreate(
-                    ['id' => $item['id']],
-                    $item
-                );
+                foreach ($gc_list_fetch['data'] as $item) {
+                    QrCreation::firstOrCreate(
+                        ['id' => $item['id']],
+                        $item
+                    );
+                }
+            }else{
+                return;
             }
-        }else{
-            return;
+        }catch(MaxAttemptsExceededException $e){
+            $this->release(5);
         }
     }
 }
