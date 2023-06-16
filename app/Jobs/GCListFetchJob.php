d5b052d7d80d1da83586e97a5a438b5aebe56ae0
patrickDigits <patrickpunzalan@digits.ph>
2023-06-16 17:14:26 +0800

    add url

--- app/Jobs/GCListFetchJob.php
+++ app/Jobs/GCListFetchJob.php
@@ -95,7 +95,7 @@ class GCListFetchJob implements ShouldQueue
             // Localhost fetch campaign
             $response = Http::withHeaders([
                 'Content-Type' => 'application/json',
-            ])->post('http://127.0.0.1:8080/api/get-token', [
+            ])->post('http://egc.digits.com.ph/api/get-token', [
                 'secret' => '84aad301b67368285f7b6f17eed0a064',
             ]);
 
@@ -103,7 +103,7 @@ class GCListFetchJob implements ShouldQueue
 
             $redemption_list = Http::withHeaders([
                 'Authorization' => 'Bearer ' . $get_token['data']['access_token'],
-            ])->get('http://127.0.0.1:8080/api/qr_creation');
+            ])->get('http://egc.digits.com.ph/api/qr_creation');
 
             $gc_list_fetch = $redemption_list->json();
             
