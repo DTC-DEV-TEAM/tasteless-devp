ccbf007f1c052a6f44ea96655ba6f20a981c7491
patrickDigits <patrickpunzalan@digits.ph>
2023-07-20 10:21:11 +0800

    add campaign approval

--- app/Http/Controllers/AdminQrCreationsController.php
+++ app/Http/Controllers/AdminQrCreationsController.php
@@ -1,5 +1,6 @@
 <?php namespace App\Http\Controllers;
 
+use App\CampaignStatus;
 use App\CompanyId;
 use App\IdType;
 use Session;
@@ -19,6 +20,7 @@ use Illuminate\Support\Facades\Session as UserSession;
 use App\Mail\QrEmail;
 use App\Jobs\SendEmailJob;
 use App\StoreConcept;
+use App\StoreLogo;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Validator;
 
@@ -65,6 +67,7 @@ use Illuminate\Support\Facades\Validator;
 			$this->col[] = ["label"=>"Company Name","name"=>"company_id","join"=>"company_ids,company_name"];
 			$this->col[] = ["label"=>"Created By","name"=>"created_by","join"=>"cms_users,name"];
 			$this->col[] = ["label"=>"Created At","name"=>"created_at"];
+			$this->col[] = ["label"=>"Campaign Status","name"=>"campaign_status"];
 			# END COLUMNS DO NOT REMOVE THIS LINE
 
 			# START FORM DO NOT REMOVE THIS LINE
@@ -105,6 +108,16 @@ use Illuminate\Support\Facades\Validator;
 			| 
 			*/
 			$this->addaction = array();
+
+			if(CRUDBooster::myPrivilegeName() == 'Head'){
+				$this->addaction[] = ['title'=>'Edit','url'=>CRUDBooster::mainpath('edit/[id]'),'icon'=>'fa fa-pencil', "showIf"=>"[campaign_status] == 1"];
+			}elseif(CRUDBooster::myPrivilegeName() == 'Accounting'){
+				$this->addaction[] = ['title'=>'Edit','url'=>CRUDBooster::mainpath('edit/[id]'),'icon'=>'fa fa-pencil', "showIf"=>"[campaign_status] == 2"];
+			}elseif(CRUDBooster::myPrivilegeName() == 'Admin'){
+				return;
+			}else{
+				$this->addaction[] = ['title'=>'Edit','url'=>CRUDBooster::mainpath('edit/[id]'),'icon'=>'fa fa-pencil'];
+			}
 
 
 			/* 
@@ -262,8 +275,18 @@ use Illuminate\Support\Facades\Validator;
 		|
 		*/
 		public function hook_query_index(&$query) {
-			//Your code here
-				
+			//Your code here	
+			$query->orderByRaw("
+			CASE
+				WHEN campaign_status = 'campaign_status' THEN 1
+				WHEN campaign_status = 3 THEN 2
+				WHEN campaign_status = 1 THEN 3
+				WHEN campaign_status = 2 THEN 4
+				WHEN campaign_status = 4 THEN 5
+				ELSE 6
+			END
+		");
+		
 		}
 
 		/*
@@ -274,6 +297,21 @@ use Illuminate\Support\Facades\Validator;
 		*/    
 		public function hook_row_index($column_index,&$column_value) {	        
 			//Your code here
+			if($column_index == '10'){
+				if($column_value == '1'){
+					$column_value = '<span class="label" style="background-color: rgb(34 211 238); color: white; font-size: 12px;">FOR APPROVAL</span>';
+				}else if($column_value == '2'){
+					$column_value = '<span class="label" style="background-color: rgb(251 146 60);
+					; color: white; font-size: 12px;">FOR ACCOUNTING APPROVAL</span>';
+				}
+				else if($column_value == '3'){
+					$column_value = '<span class="label" style="background-color: rgb(74 222 128);
+					; color: white; font-size: 12px;">APPROVED</span>';
+				}else{
+					$column_value = '<span class="label" style="background-color: rgb(239 68 68);
+					; color: white; font-size: 12px;">REJECTED</span>';
+				}
+			}
 		}
 
 		/*
@@ -448,13 +486,41 @@ use Illuminate\Support\Facades\Validator;
 			
 			$company = CompanyId::get();
 			$store = StoreConcept::get();
+			$store_logo = StoreLogo::get();
 
 			$data = [];
 			$data['page_title'] = 'Add Data';
 			$data['company_id'] = $company;
 			$data['stores'] = $store;
+			$data['store_logo'] = $store_logo;
+
 			// dd($data['company_id']);
 			//Please use view method instead view method from laravel
+			return $this->view('redeem_qr.add_campaign',$data);
+		}
+
+		public function getEdit($id) {
+			
+			$qr_creation = DB::table('qr_creations')->where('qr_creations.id', $id)
+				->leftJoin('company_ids', 'qr_creations.company_id', 'company_ids.id')
+				->leftJoin('campaign_statuses', 'qr_creations.campaign_status', 'campaign_statuses.id')
+				->leftJoin('cms_users as manager', 'qr_creations.manager_approval', 'manager.id')
+				->leftJoin('cms_users as accounting', 'qr_creations.accounting_approval', 'accounting.id')
+				->select('qr_creations.*','company_ids.company_name', 'manager.name as manager_name', 'accounting.name as accounting_name', 'campaign_statuses.name as campaign_status_name')
+				->get()
+				->first();
+
+			$company = CompanyId::get();
+			$store = StoreConcept::get();
+			$store_logo = StoreLogo::get();
+
+			$data = [];
+			$data['page_title'] = 'Detail Campaign Creation';
+			$data['company_id'] = $company;
+			$data['stores'] = $store;
+			$data['qr_creation'] = $qr_creation;
+			$data['store_logo'] = $store_logo;
+
 			return $this->view('redeem_qr.add_campaign',$data);
 		}
 
@@ -462,18 +528,24 @@ use Illuminate\Support\Facades\Validator;
 			
 			$qr_creation = DB::table('qr_creations')->where('qr_creations.id', $id)
 				->leftJoin('company_ids', 'qr_creations.company_id', 'company_ids.id')
-				->select('qr_creations.*','company_ids.company_name')
-				->get()->first();
+				->leftJoin('campaign_statuses', 'qr_creations.campaign_status', 'campaign_statuses.id')
+				->leftJoin('cms_users as manager', 'qr_creations.manager_approval', 'manager.id')
+				->leftJoin('cms_users as accounting', 'qr_creations.accounting_approval', 'accounting.id')
+				->select('qr_creations.*','company_ids.company_name', 'manager.name as manager_name', 'accounting.name as accounting_name', 'campaign_statuses.name as campaign_status_name')
+				->get()
+				->first();
 
 			// dd($qr_creation);
 			$company = CompanyId::get();
 			$store = StoreConcept::get();
-		
+			$store_logo = StoreLogo::get();
+
 			$data = [];
 			$data['page_title'] = 'Detail Campaign Creation';
 			$data['company_id'] = $company;
 			$data['stores'] = $store;
 			$data['qr_creation'] = $qr_creation;
+			$data['store_logo'] = $store_logo;
 
 			return $this->view('redeem_qr.add_campaign',$data);
 		}
@@ -481,7 +553,8 @@ use Illuminate\Support\Facades\Validator;
 		public function addCampaign(IlluminateRequest $request){
 			
 			$campaign = $request->all();
-
+			$excel_file = $campaign['po_attachment'];
+			
 			$request->validate([
 				'campaign_id' => 'required|unique:qr_creations'
 			]);
@@ -491,9 +564,61 @@ use Illuminate\Support\Facades\Validator;
 			$campaign['created_by'] = CRUDBooster::myId();
 			$campaign['created_at'] = date('Y-m-d H:i:s');
 			
-			QrCreation::insert(Arr::except($campaign , ['_token','stores']));
+			$campaign_information = new QrCreation(Arr::except($campaign , ['_token','stores','po_attachment','id']));
+			$campaign_information->save();
+			$campaign_information->campaign_status = 1;
+			$campaign_information->save();
+
+			if($excel_file){
+
+				$campaign_information->po_attachment = $campaign_information->campaign_id.'.'.$excel_file->getClientOriginalExtension();
+				$campaign_information->save();
+				$excel_file->move(public_path('uploaded_po/file/'),$campaign_information->po_attachment);
+			}
 
 			return CRUDBooster::redirect(CRUDBooster::mainpath(), 'Campaign Creation added succesfully', 'success')->send();
+		}
+
+		public function campaignApproval(IlluminateRequest $request){
+
+			$qr_creation = QrCreation::find($request->get('id'));
+			
+			if($qr_creation->campaign_status == 1){
+				if($request->get('approve')){
+
+					$qr_creation->update([
+						'manager_approval' => CRUDBooster::myId(),
+						'campaign_status' => 2,
+						'manager_approval_date' => date('Y-m-d H:i:s')
+					]);
+				}else{
+
+					$qr_creation->update([
+						'manager_approval' => CRUDBooster::myId(),
+						'campaign_status' => 4,
+						'accounting_approval_date' => date('Y-m-d H:i:s')
+					]);
+				}
+			}elseif($qr_creation->campaign_status == 2){
+				if($request->get('approve')){
+
+					$qr_creation->update([
+						'accounting_approval' => CRUDBooster::myId(),
+						'campaign_status' => 3,
+						'billing_number' => $request->get('billing_number'),
+						'accounting_approval_date' => date('Y-m-d H:i:s')
+					]);
+				}else{
+
+					$qr_creation->update([
+						'accounting_approval' => CRUDBooster::myId(),
+						'campaign_status' => 4,
+						'accounting_approval_date' => date('Y-m-d H:i:s')
+					]);
+				}
+			}
+
+			return CRUDBooster::redirect(CRUDBooster::mainpath(), 'Campaign Creation successfully updated', 'success')->send();
 
 		}
 
