c3358b7efa04efa6130760aecec7b7de96e3d788
patrickDigits <patrickpunzalan@digits.ph>
2023-07-20 10:50:51 +0800

    add approver and accounting approver

--- resources/views/redeem_qr/add_campaign.blade.php
+++ resources/views/redeem_qr/add_campaign.blade.php
@@ -54,12 +54,71 @@
             border: 1px solid #c7c5c5;
         }
 
-        select:invalid {
+        /* select:invalid {
             color: #999;
+        } */
+
+        .for_approval_head{
+            padding: 5px 10px;
+            border-radius: 5px;
+            background-color: rgb(34 211 238);
+            color: white;
+        }
+
+        .approve{
+            padding: 5px 10px;
+            border-radius: 5px;
+            background-color: rgb(74 222 128);
+            color: white;
+        }
+
+        .approve_accounting{
+            padding: 5px 10px;
+            border-radius: 5px;
+            background-color: rgb(251 146 60);
+            color: white;
+        }
+
+        .rejected{
+            padding: 5px 10px;
+            border-radius: 5px;
+            background-color: rgb(239 68 68);
+            color: white;
+        }
+
+        .for_approval{
+            display: flex;
+            margin: 3px 20px;
+            flex-wrap: wrap;
+
+        }
+
+        .for_approval_content{
+            margin-right: 20px;
+            margin-bottom: 20px;
+        }
+
+        .for_approval_content .label_status{
+            display: block;
+            margin-bottom: 9px;
+        }
+
+        input[name="billing_number"]{
+            height: 35px;
+            border: 1px solid #c7c5c5;
+            width: 100%;
+            max-width: 185px;
+            padding: 0 5px;
+            outline: none;
+            text-align: center;
+        }
+
+        input[name="billing_number"]:focus{
+            border: 2px solid #605CA8;
         }
 
         .file-upload-content input[type="file"]::after {
-            content: "Choose an PDF File";
+            content: "Choose a PDF File";
         }
 
         .select2-container--default .select2-selection--multiple .select2-selection__choice {
@@ -76,16 +135,9 @@
         }
 
         .swal2-popup {
-            /* padding: 100px 0; */
             font-size: 17px !important;
             color: rgb(0, 0, 0) !important;
         }
-
-        /* @media only screen and (max-width: 600px) {
-            .form-input-content{
-                padding-left: 0px;
-            }
-        } */
     </style>
 @endpush
 @section('content')
@@ -95,8 +147,8 @@
         <div class='panel-heading'>Add Form</div>
         <form method='post' action='{{ route('add_campaign') }}' autocomplete="off" enctype="multipart/form-data">
             @csrf
+            <input type="text" class="hide" name="id" value="{{ $qr_creation->id }}">
             <div class='panel-body'>
-
                 <div class="form-input-content">
                     <div class="form-inputs">
                         <label for="">Campaign ID:</label>
@@ -121,7 +173,7 @@
                     <div class="form-inputs">
                         <label for="">Company Name:</label>
                         <select id="company_select" name="company_id" required>
-                            <option value="" disabled required selected>Select Company Tag</option>
+                            <option value="" disabled required selected></option>
                             @foreach ($company_id as $company)
                             <option value="{{ $company->id }}" {{ $qr_creation->company_id == $company->id ? 'selected':'' }}>{{ $company->company_name }}</option>
                             @endforeach
@@ -155,12 +207,34 @@
                     <div class="form-inputs">
                         <label for="">Store Logo:</label>
                         <select id="company_select" name="store_logo" required>
-                            <option value="" disabled required selected>Select Logo</option>
+                            <option value="" disabled required selected></option>
                             @foreach ($store_logo as $store)
                             <option value="{{ $store->id }}" {{ $qr_creation->store_logo == $store->id ? 'selected':'' }}>{{ $store->name }}</option>
                             @endforeach
                         </select>
                     </div>
+                </div>
+                <div class="for_approval">
+                    <div class="for_approval_content">
+                        @if($qr_creation->campaign_status)
+                        <label for="" class="label_status">Status</label>
+                        @endif
+                        @if ($qr_creation->campaign_status == 1)            
+                        <span class="for_approval_head">{{ $qr_creation->campaign_status_name }}</span>
+                        @elseif($qr_creation->campaign_status == 2)
+                        <span class="approve_accounting">{{ $qr_creation->campaign_status_name }}</span>
+                        @elseif($qr_creation->campaign_status == 3)
+                        <span class="approve">{{ $qr_creation->campaign_status_name }}</span>
+                        @elseif($qr_creation->campaign_status == 4)
+                        <span class="rejected">{{ $qr_creation->campaign_status_name }}</span>
+                        @endif
+                    </div>
+                    @if ($qr_creation->campaign_status == 2 || $qr_creation->campaign_status == 3)     
+                    <div class="for_approval_content" style="max-width: 200px;">
+                        <label for="">Billing Number</label>
+                        <input type="text" name="billing_number" placeholder="Input billing number" value="{{ $qr_creation->billing_number }}" {{ (Request::segment(3) == 'detail') ? 'disabled' : '' }} required>
+                    </div>
+                    @endif
                 </div>
                 <div class="form-inputs">
                     @if($errors->has('campaign_id'))
@@ -178,7 +252,16 @@
             <div class='panel-footer'>
                 <a href="{{ CRUDBooster::mainpath() }}" class="btn btn-default" style="border: 1px solid #ddd;">Cancel</a>
                 <input type='submit' class='btn btn-primary hide' id="submit_form" value='Save changes'/>
+                <input type='submit' class='btn btn-primary hide' id="reject_form" name="reject" value='Reject'/>
+                <input type='submit' class='btn btn-primary hide' id="approve_form" name="approve" value='Approve'/>
+                @if($qr_creation->campaign_status == null)
                 <button type="button" class='btn btn-primary' id="submit_btn">Submit</button>
+                @else
+                @if (!(Request::segment(3) == 'detail') && ($qr_creation->campaign_status != 3 && $qr_creation->campaign_status != 4))          
+                <button type="button" class='btn btn-success' style="float: right; margin-left: 5px;" id="approve_btn">Approve</button>
+                <button type="button" class='btn btn-danger' style="float: right;" id="reject_btn">Reject</button>
+                @endif
+                @endif
             </div>
         </form>
     </div>
@@ -192,7 +275,7 @@
                     
                     $('.panel-heading').text('Details');
                     $('input').css('background-color', '#eee');
-                    $('input').attr('disabled', true);
+                    $('input').attr('readonly', true);
                     $('select').css('background-color', '#eee');
                     $('select').attr('disabled', true);
                     $('input[name="campaign_id"]').val('{{ $qr_creation->campaign_id }}');
@@ -201,7 +284,11 @@
                     $('input[name="batch_number"]').val('{{ $qr_creation->batch_number }}');
                     $('input[name="batch_group"]').val('{{ $qr_creation->batch_group }}');
                     $('input[name="po_number"]').val('{{ $qr_creation->po_number }}');
-                    $('#submit_btn').hide();
+                }
+
+                if("{{ $qr_creation->campaign_status == 2 }}"){
+                    $('input[name="billing_number"]').attr('readonly', false);
+                    $('input[name="billing_number"]').css('background-color', '#fff');
                 }
 
             }
@@ -225,6 +312,42 @@
                 }).then((result) => {
                     if (result.isConfirmed) {
                         $('#submit_form').click();
+                    }
+                })
+            });
+
+            $('#reject_btn').click(function(){
+                Swal.fire({
+                    title: 'Are you sure?',
+                    text: "You won't be able to revert this!",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Yes, submit it!',
+                    returnFocus: false,
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        $('form').attr("action", "{{ route('campaign_approval') }}");
+                        $('#reject_form').click();
+                    }
+                })
+            });
+
+            $('#approve_btn').click(function(){
+                Swal.fire({
+                    title: 'Are you sure?',
+                    text: "You won't be able to revert this!",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Yes, submit it!',
+                    returnFocus: false,
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        $('form').attr("action", "{{ route('campaign_approval') }}");
+                        $('#approve_form').click();
                     }
                 })
             });
