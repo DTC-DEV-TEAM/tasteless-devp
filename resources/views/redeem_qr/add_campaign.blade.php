827f407d608d310d88b079c9edd052dd1b7489e2
patrickDigits <patrickpunzalan@digits.ph>
2023-08-16 13:32:53 +0800

    loading

--- resources/views/redeem_qr/add_campaign.blade.php
+++ resources/views/redeem_qr/add_campaign.blade.php
@@ -138,10 +138,104 @@
             font-size: 17px !important;
             color: rgb(0, 0, 0) !important;
         }
+
+        /* Loading Animation */
+        .sk-chase-position{
+            position: absolute;
+            top: 47%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            height: 100%;
+            width: 100%;
+            display: grid;
+            place-items: center;
+            z-index: 9999;
+            background-color: #21212172;
+        }
+
+        .sk-chase {
+            width: 100px;
+            height: 100px;
+            position: relative;
+            animation: sk-chase 2.5s infinite linear both;
+        }
+
+        .sk-chase-text{
+            position: absolute;
+            top: 60%;
+            color: white;
+        }
+
+        .sk-chase-text{
+            font-weight: bold;
+            font-size: 15px;
+        }
+
+        .sk-chase-dot {
+            width: 100%;
+            height: 100%;
+            position: absolute;
+            left: 0;
+            top: 0; 
+            animation: sk-chase-dot 2.0s infinite ease-in-out both; 
+        }
+
+        .sk-chase-dot:before {
+            content: '';
+            display: block;
+            width: 25%;
+            height: 25%;
+            background-color: #fff;
+            border-radius: 100%;
+            animation: sk-chase-dot-before 2.0s infinite ease-in-out both; 
+        }
+
+        .sk-chase-dot:nth-child(1) { animation-delay: -1.1s; }
+        .sk-chase-dot:nth-child(2) { animation-delay: -1.0s; }
+        .sk-chase-dot:nth-child(3) { animation-delay: -0.9s; }
+        .sk-chase-dot:nth-child(4) { animation-delay: -0.8s; }
+        .sk-chase-dot:nth-child(5) { animation-delay: -0.7s; }
+        .sk-chase-dot:nth-child(6) { animation-delay: -0.6s; }
+        .sk-chase-dot:nth-child(1):before { animation-delay: -1.1s; }
+        .sk-chase-dot:nth-child(2):before { animation-delay: -1.0s; }
+        .sk-chase-dot:nth-child(3):before { animation-delay: -0.9s; }
+        .sk-chase-dot:nth-child(4):before { animation-delay: -0.8s; }
+        .sk-chase-dot:nth-child(5):before { animation-delay: -0.7s; }
+        .sk-chase-dot:nth-child(6):before { animation-delay: -0.6s; }
+
+        @keyframes sk-chase {
+        100% { transform: rotate(360deg); } 
+        }
+
+        @keyframes sk-chase-dot {
+        80%, 100% { transform: rotate(360deg); } 
+        }
+
+        @keyframes sk-chase-dot-before {
+        50% {
+            transform: scale(0.4); 
+        } 100%, 0% {
+            transform: scale(1.0); 
+        } 
+        }
     </style>
 @endpush
 @section('content')
-  <!-- Your html goes here -->
+    <!-- Your html goes here -->
+    <div class="sk-chase-position" style="display: none;">
+        <div class="sk-chase">
+            <div class="sk-chase-dot"></div>
+            <div class="sk-chase-dot"></div>
+            <div class="sk-chase-dot"></div>
+            <div class="sk-chase-dot"></div>
+            <div class="sk-chase-dot"></div>
+            <div class="sk-chase-dot"></div>
+        </div>
+        <div class="sk-chase-text">
+            <p>Please wait, system is on process...</p>
+        </div>
+    </div>
+
     <p><a title='Return' href='{{ CRUDBooster::mainpath() }}'><i class='fa fa-chevron-circle-left '></i>&nbsp; Back To Campaign Creation Home</a></p>
     <div class='panel panel-default'>
         <div class='panel-heading'>Add Form</div>
@@ -335,6 +429,7 @@
             });
 
             $('#approve_btn').click(function(){
+                // console.log($("input[name='billing_number']").val() == '');
                 Swal.fire({
                     title: 'Are you sure?',
                     text: "You won't be able to revert this!",
@@ -347,6 +442,9 @@
                 }).then((result) => {
                     if (result.isConfirmed) {
                         $('form').attr("action", "{{ route('campaign_approval') }}");
+                        if($("input[name='billing_number']").val() != ''){
+                            $('.sk-chase-position').show();
+                        }
                         $('#approve_form').click();
                     }
                 })
