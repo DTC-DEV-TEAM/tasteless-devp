63d64cad1a95ef98cb7c59c7f88a78766df8a8d2
patrickDigits <patrickpunzalan@digits.ph>
2023-10-11 18:18:26 +0800

    remove jquery script

--- resources/views/redeem_qr/qr_redeem_section.blade.php
+++ resources/views/redeem_qr/qr_redeem_section.blade.php
@@ -2,7 +2,7 @@
 @extends('crudbooster::admin_template')
 
 @push('head')
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+  {{-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script> --}}
   <link rel="stylesheet" href="{{ asset('css/style.css') }}">
   <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
   <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
